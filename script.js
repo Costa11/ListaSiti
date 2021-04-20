6c255ab3ff8e809241ff292ba54e6d9e5b678399
Costa11 <>
2021-04-20 10:44:18 +0000

    Completed

---
+++ script.js
@@ -0,0 +1,34 @@
+const sites = [];
+
+const addSite = function (event) {
+
+  event.preventDefault();
+  const url = $("#form-url").val();
+  const title = $("#form-title").val();
+  const category = $("#form-category").val();
+
+  console.log("URL: " + url);
+  console.log("Titolo: " + title);
+  console.log("Categoria:" + category);
+  
+  const newSite = {
+    url: url,
+    title: title,
+    category: category
+  };
+  
+  sites.push(newSite);
+  console.log(sites);
+  writeSiteList(sites);
+}
+const writeSiteList = function (site) {
+  const aElem = $("<a>").attr("href", site[site.length - 1].url).text(site[site.length - 1].title);
+  const liElem = $("<li>").append(aElem);
+  $("#site-list").append(liElem);
+}
+
+const init = function () {
+  $("form").submit(addSite);
+}
+
+$(document).ready(init);
